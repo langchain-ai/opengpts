bb867e66bc8182e13559fa624d81e09cc5a40ab8
Nuno Campos <nuno@boringbits.io>
2023-11-06 11:35:13 +0000

    Remove use of unreleased storage

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,16 +1,10 @@
-import aiosqlite
 from fastapi import FastAPI
 from langserve import add_routes
 from gizmo_agent import agent
 
 app = FastAPI()
 
-add_routes(
-    app,
-    agent,
-    config_keys=["configurable"],
-    storage=aiosqlite.connect("./langserve.sqlite"),
-)
+add_routes(app, agent, config_keys=["configurable"])
 
 if __name__ == "__main__":
     import uvicorn
