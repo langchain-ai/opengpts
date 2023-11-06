7c07dbb8836bd3cdc3c0c4bde1fc8fbeaf6da285
Harrison Chase <hw.chase.17@gmail.com>
2023-11-06 10:09:11 -0800

    bearly

--- backend/packages/gizmo-agent/gizmo_agent/tools.py
+++ backend/packages/gizmo-agent/gizmo_agent/tools.py
@@ -1,6 +1,9 @@
+import os
 from langchain.tools import DuckDuckGoSearchRun
 from langchain_experimental.tools import PythonREPLTool
 from langchain.pydantic_v1 import BaseModel, Field
+from langchain.tools import BearlyInterpreterTool
+
 
 
 class DDGInput(BaseModel):
@@ -11,9 +14,13 @@ class PythonREPLInput(BaseModel):
     query: str = Field(description="python command to run")
 
 
+bearly_tool = BearlyInterpreterTool(api_key=os.environ["BEARLY_API_KEY"])
+
+
 TOOLS = [
     DuckDuckGoSearchRun(args_schema=DDGInput),
     PythonREPLTool(args_schema=PythonREPLInput),
+    bearly_tool.as_tool()
 ]
 
 TOOL_OPTIONS = {tool.name: tool for tool in TOOLS}
