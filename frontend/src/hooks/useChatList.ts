e61caa600caed38aa148b224a0dc42050ea1e3d5
Nuno Campos <nuno@boringbits.io>
2023-11-06 11:49:22 +0000

    Store chat list in local storage

--- frontend/src/hooks/useChatList.ts
+++ frontend/src/hooks/useChatList.ts
@@ -1,4 +1,5 @@
 import { useCallback, useState } from "react";
+import { useStatePersist } from "./useStatePersist";
 
 export interface Message {
   type: string;
@@ -23,28 +24,33 @@ export interface ChatListProps {
 }
 
 export function useChatList(): ChatListProps {
-  // TODO store server-side
-  const [chats, setChats] = useState<Chat[]>([]);
+  const [chats, setChats] = useStatePersist<Chat[]>([], "chats");
   const [current, setCurrent] = useState<string | null>(null);
 
-  const createChat = useCallback(async (name: string, messages: Message[]) => {
-    const chat = {
-      id: Math.random().toString(36).substr(2, 9),
-      name,
-      created_at: new Date().toISOString(),
-      updated_at: new Date().toISOString(),
-      messages: messages,
-    };
-    setChats((chats) => [...chats, chat]);
-    setCurrent(chat.id);
-    return chat;
-  }, []);
+  const createChat = useCallback(
+    async (name: string, messages: Message[]) => {
+      const chat = {
+        id: Math.random().toString(36).substr(2, 9),
+        name,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+        messages: messages,
+      };
+      setChats((chats) => [...chats, chat]);
+      setCurrent(chat.id);
+      return chat;
+    },
+    [setChats]
+  );
 
-  const updateChat = useCallback(async (id: string, update: Partial<Chat>) => {
-    setChats((chats) =>
-      chats.map((chat) => (chat.id === id ? { ...chat, ...update } : chat))
-    );
-  }, []);
+  const updateChat = useCallback(
+    async (id: string, update: Partial<Chat>) => {
+      setChats((chats) =>
+        chats.map((chat) => (chat.id === id ? { ...chat, ...update } : chat))
+      );
+    },
+    [setChats]
+  );
 
   const enterChat = useCallback((id: string | null) => {
     setCurrent(id);
