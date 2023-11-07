0295ba2cb3f7bdf5beeef7d8880138f2bbb75d79
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2023-11-07 17:53:09 +0200

    Adding Tavily Search Tool

--- backend/packages/gizmo-agent/gizmo_agent/tools.py
+++ backend/packages/gizmo-agent/gizmo_agent/tools.py
@@ -1,4 +1,6 @@
 from langchain.tools import DuckDuckGoSearchRun
+from langchain.tools.tavily_search import TavilySearchResults
+from langchain.utilities.tavily_search import TavilySearchAPIWrapper
 from langchain_experimental.tools import PythonREPLTool
 from langchain.pydantic_v1 import BaseModel, Field
 
@@ -6,13 +8,13 @@ from langchain.pydantic_v1 import BaseModel, Field
 class DDGInput(BaseModel):
     query: str = Field(description="search query to look up")
 
-
 class PythonREPLInput(BaseModel):
     query: str = Field(description="python command to run")
 
 
 TOOLS = [
     DuckDuckGoSearchRun(args_schema=DDGInput),
+    TavilySearchResults(api_wrapper=TavilySearchAPIWrapper()),
     # PythonREPLTool(args_schema=PythonREPLInput),
 ]
 
