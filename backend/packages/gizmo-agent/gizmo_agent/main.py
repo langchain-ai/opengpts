7e67f1470f984caa558cfaf1d0f33e6f17e485c0
Nuno Campos <nuno@boringbits.io>
2023-11-10 10:02:35 +0000

    Fix gpt4

--- backend/packages/gizmo-agent/gizmo_agent/main.py
+++ backend/packages/gizmo-agent/gizmo_agent/main.py
@@ -53,7 +53,7 @@ class ConfigurableAgent(RunnableBinding):
                 _tools.append(TOOLS[_tool])
         if agent == GizmoAgentType.GPT_35_TURBO:
             _agent = get_openai_function_agent(_tools, system_message)
-        elif agent == GizmoAgentType.GPT_35_TURBO:
+        elif agent == GizmoAgentType.GPT_4:
             _agent = get_openai_function_agent(_tools, system_message, gpt_4=True)
         # elif agent == GizmoAgentType.AZURE_OPENAI:
         #     _agent = get_openai_function_agent(tools, system_message, azure=True)
