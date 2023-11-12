0ac1dc7a72dc7a0fc2a9cc34934201ea014b342f
Nuno Campos <nuno@boringbits.io>
2023-11-12 22:48:07 +0000

    Disable autofocus on new bot name

--- frontend/src/components/Config.tsx
+++ frontend/src/components/Config.tsx
@@ -341,7 +341,6 @@ export function Config(props: {
                 type="text"
                 name="key"
                 id="key"
-                autoFocus
                 autoComplete="off"
                 className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-4 text-gray-900 ring-1 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ring-inset ring-gray-300"
                 placeholder="Name your bot"
