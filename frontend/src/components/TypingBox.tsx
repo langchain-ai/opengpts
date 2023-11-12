ea875ce66c2acc7f8fb3abfb49b7ca8d3636ab07
Nuno Campos <nuno@boringbits.io>
2023-11-12 22:40:27 +0000

    Use disabled var

--- frontend/src/components/TypingBox.tsx
+++ frontend/src/components/TypingBox.tsx
@@ -15,11 +15,11 @@ export default function TypingBox(props: {
     <form
       className={cn(
         "mt-2 flex rounded-md shadow-sm",
-        props.disabled && "opacity-50 cursor-not-allowed"
+        disabled && "opacity-50 cursor-not-allowed"
       )}
       onSubmit={async (e) => {
         e.preventDefault();
-        if (props.disabled) return;
+        if (disabled) return;
         const form = e.target as HTMLFormElement;
         const message = form.message.value;
         setInflight(true);
@@ -43,12 +43,12 @@ export default function TypingBox(props: {
           autoComplete="off"
           className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="Send a message"
-          readOnly={props.disabled}
+          readOnly={disabled}
         />
       </div>
       <button
         type="submit"
-        disabled={props.disabled}
+        disabled={disabled}
         className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 bg-white"
       >
         <PaperAirplaneIcon
