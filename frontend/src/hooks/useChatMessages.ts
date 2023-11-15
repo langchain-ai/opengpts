1652c940a323a8b75758b0bf95511c221eb613ba
Nuno Campos <nuno@boringbits.io>
2023-11-15 14:32:46 +0000

    Lint

--- frontend/src/hooks/useChatMessages.ts
+++ frontend/src/hooks/useChatMessages.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { Message } from "./useChatList";
 import { StreamState } from "./useStreamState";
 
