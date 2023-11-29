31ba3ea1e7327143d59ee07dac06c60e72057784
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-11-29 10:27:55 -0500

    add frontend to docker-compose.yml

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: 80,
     proxy: {
       "^/(assistants|threads|ingest|runs)": {
-        target: "http://127.0.0.1:8100",
+        target: "http://backend:8100",
         changeOrigin: true,
         rewrite: (path) => path.replace("/____LANGSERVE_BASE_URL", ""),
       },
