0c5bb389fc5172caf8e2b3616d21cf6960b416e5
zhenjie.ye <yezhenjie@outlook.de>
2023-11-29 14:31:07 +0800

    add AzureOpenAI Embedding Support

--- backend/packages/gizmo-agent/gizmo_agent/ingest.py
+++ backend/packages/gizmo-agent/gizmo_agent/ingest.py
@@ -2,6 +2,7 @@ import os
 
 from agent_executor.upload import IngestRunnable
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings.azure_openai import AzureOpenAIEmbeddings
 from langchain.schema.runnable import ConfigurableField
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.redis import Redis
@@ -9,16 +10,28 @@ from langchain.vectorstores.redis import Redis
 index_schema = {
     "tag": [{"name": "namespace"}],
 }
+
+if os.environ("OPENAI_API_TYPE") == "azure":
+    embeddings = OpenAIEmbeddings(
+        deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME_EB"],
+        model=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME_EB"],
+        openai_api_base=os.environ["AZURE_OPENAI_API_BASE"],
+        openai_api_type="azure",
+    )
+else:
+    embeddings = OpenAIEmbeddings()
+
 vstore = Redis(
     redis_url=os.environ["REDIS_URL"],
     index_name="opengpts",
-    embedding=OpenAIEmbeddings(),
+    embedding=embeddings,
     index_schema=index_schema,
 )
 
 
 ingest_runnable = IngestRunnable(
-    text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200),
+    text_splitter=RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200),
     vectorstore=vstore,
 ).configurable_fields(
     assistant_id=ConfigurableField(
