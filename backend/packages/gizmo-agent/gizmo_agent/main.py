34e6ae756e16dcb4065f9aa644791e9d981e8dd2
Nuno Campos <nuno@boringbits.io>
2023-12-13 20:44:00 -0800

    Disable claude

--- backend/packages/gizmo-agent/gizmo_agent/main.py
+++ backend/packages/gizmo-agent/gizmo_agent/main.py
@@ -57,10 +57,10 @@ class ConfigurableAgent(RunnableBinding):
         #     _agent = get_openai_function_agent(_tools, system_message, gpt_4=True)
         # elif agent == GizmoAgentType.AZURE_OPENAI:
         #     _agent = get_openai_function_agent(_tools, system_message, azure=True)
-        elif agent == GizmoAgentType.CLAUDE2:
-            _agent = get_xml_agent(_tools, system_message)
-        elif agent == GizmoAgentType.BEDROCK_CLAUDE2:
-            _agent = get_xml_agent(_tools, system_message, bedrock=True)
+        # elif agent == GizmoAgentType.CLAUDE2:
+        #     _agent = get_xml_agent(_tools, system_message)
+        # elif agent == GizmoAgentType.BEDROCK_CLAUDE2:
+        #     _agent = get_xml_agent(_tools, system_message, bedrock=True)
         else:
             raise ValueError("Unexpected agent type")
         agent_executor = get_agent_executor(
