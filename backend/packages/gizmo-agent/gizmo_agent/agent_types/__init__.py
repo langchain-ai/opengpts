b0bf1b17c6803ffccbe7a5826932cdb59d029366
Alex W <alex@blinkenlights.ca>
2024-01-05 11:26:03 -0500

    Initial testing of Command as an LLM backend for OpenGPTs

--- backend/packages/gizmo-agent/gizmo_agent/agent_types/__init__.py
+++ backend/packages/gizmo-agent/gizmo_agent/agent_types/__init__.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from .openai import get_openai_function_agent
+from .cohere import get_cohere_function_agent
 from .xml.agent import get_xml_agent
 
 
@@ -10,10 +11,12 @@ class GizmoAgentType(str, Enum):
     # AZURE_OPENAI = "GPT 4 (Azure OpenAI)"
     CLAUDE2 = "Claude 2"
     BEDROCK_CLAUDE2 = "Claude 2 (Amazon Bedrock)"
+    COHERE_COMMAND = "Command (Cohere)"
 
 
 __all__ = [
     "get_openai_function_agent",
+    "get_cohere_function_agent",
     "get_xml_agent",
     "GizmoAgentType",
 ]
