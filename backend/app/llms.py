13eaf214a873f9ff5423fc1ef8a8a5fbb418b4a7
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-02-09 15:03:19 +0600

    add proxy option

--- backend/app/llms.py
+++ backend/app/llms.py
@@ -1,6 +1,6 @@
 import os
 from functools import lru_cache
-
+import httpx
 import boto3
 from langchain_community.chat_models import BedrockChat, ChatAnthropic, ChatFireworks
 from langchain_google_vertexai import ChatVertexAI
@@ -9,13 +9,19 @@ from langchain_openai import AzureChatOpenAI, ChatOpenAI
 
 @lru_cache(maxsize=4)
 def get_openai_llm(gpt_4: bool = False, azure: bool = False):
+    proxy_url = os.environ["PROXY_URLS"]
+    if proxy_url is not None or proxy_url != "":
+        http_client = httpx.AsyncClient(proxies=proxy_url)
+    else:
+        http_client = None
     if not azure:
         if gpt_4:
-            llm = ChatOpenAI(model="gpt-4-1106-preview", temperature=0, streaming=True)
+            llm = ChatOpenAI(http_client=http_client, model="gpt-4-1106-preview", temperature=0, streaming=True)
         else:
-            llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0, streaming=True)
+            llm = ChatOpenAI(http_client=http_client, model="gpt-3.5-turbo-1106", temperature=0, streaming=True)
     else:
         llm = AzureChatOpenAI(
+            http_client=http_client,
             temperature=0,
             deployment_name=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
             openai_api_base=os.environ["AZURE_OPENAI_API_BASE"],
