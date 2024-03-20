55d27174d6821545d935943162afc1148d30d2b4
Jelmer Borst <jelmer.borst@teampicnic.com>
2024-03-20 10:05:39 +0100

    Use gpt-3.5-turbo

--- backend/app/llms.py
+++ backend/app/llms.py
@@ -24,7 +24,7 @@ def get_openai_llm(gpt_4: bool = False, azure: bool = False):
             logger.warn("Invalid proxy URL provided. Proceeding without proxy.")
 
     if not azure:
-        openai_model = "gpt-4-turbo-preview" if gpt_4 else "gpt-3.5-turbo-0125"
+        openai_model = "gpt-4-turbo-preview" if gpt_4 else "gpt-3.5-turbo"
         llm = ChatOpenAI(
             http_client=http_client,
             model=openai_model,
