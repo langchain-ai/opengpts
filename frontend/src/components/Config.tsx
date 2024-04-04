fa11ee3bcb684d8142becf1b7ff16e7c01c67dc7
Mikko Korpela <mikko@sema4.ai>
2024-04-04 14:21:33 +0300

    PUT / POST switch based on if we are creating or editing

--- frontend/src/components/Config.tsx
+++ frontend/src/components/Config.tsx
@@ -549,7 +549,7 @@ export function Config(props: {
             autoComplete="off"
             className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-4 text-gray-900 ring-1 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ring-inset ring-gray-300"
             placeholder="Name your bot"
-            value={props.config?.name}
+            defaultValue={props.config?.name}
           />
         </div>
         <button
@@ -585,7 +585,13 @@ export function Config(props: {
           vals.configurable["type==agent/tools"] = [...selectedTools];
           setSelectedTools([]);
         }
-        const assistantId = await props.saveConfig(key, vals!, files, isPublic);
+        const assistantId = await props.saveConfig(
+          key,
+          vals!,
+          files,
+          isPublic,
+          props.config?.assistant_id,
+        );
         props.enterConfig(assistantId);
         setInflight(false);
       }}
