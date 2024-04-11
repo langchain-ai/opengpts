8f0c5fe2df6fa89cb64454bd6c382efada61dd61
Bakar Tavadze <bakar@sema4.ai>
2024-04-11 20:08:38 +0400

    Add created_at column to the user table.

--- backend/app/schema.py
+++ backend/app/schema.py
@@ -9,6 +9,8 @@ class User(TypedDict):
     """The ID of the user."""
     sub: str
     """The sub of the user (from a JWT token)."""
+    created_at: datetime
+    """The time the user was created."""
 
 
 class Assistant(TypedDict):
