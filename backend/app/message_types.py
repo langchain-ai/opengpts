2755495e9edd1da512f4350d8be669ee6d2133f4
Nuno Campos <nuno@langchain.dev>
2024-05-08 17:57:44 -0700

    Mark custom message types as loadable

--- backend/app/message_types.py
+++ backend/app/message_types.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from langchain_core.messages import FunctionMessage, ToolMessage
+from langchain_core.load.load import ALL_SERIALIZABLE_MAPPINGS
 
 
 class LiberalFunctionMessage(FunctionMessage):
@@ -9,3 +10,16 @@ class LiberalFunctionMessage(FunctionMessage):
 
 class LiberalToolMessage(ToolMessage):
     content: Any
+
+
+# Register for deserialization
+
+ALL_SERIALIZABLE_MAPPINGS[
+    ("langchain", "schema", "messages", "LiberalFunctionMessage")
+] = ("app", "message_types", "LiberalFunctionMessage")
+
+ALL_SERIALIZABLE_MAPPINGS[("langchain", "schema", "messages", "LiberalToolMessage")] = (
+    "app",
+    "message_types",
+    "LiberalToolMessage",
+)
