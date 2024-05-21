f9a10b21190a261566bbcb94f1747a2bc964b119
Andrew Nguonly <andrewnguonly@users.noreply.github.com>
2024-05-21 14:53:00 -0700

    Update deploy version to 0.1.9. (#344)

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 set -euxo pipefail
 
-langgraph build -t langchain/opengpts-langgraph:0.1.8 --platform linux/amd64,linux/arm64
-docker push langchain/opengpts-langgraph:0.1.8
-gcloud beta run deploy opengpts-demo-langgraph --image langchain/opengpts-langgraph:0.1.8 --region us-central1 --project langchain-dev --env-vars-file .env.gcp.yaml
+langgraph build -t langchain/opengpts-langgraph:0.1.9 --platform linux/amd64,linux/arm64
+docker push langchain/opengpts-langgraph:0.1.9
+gcloud beta run deploy opengpts-demo-langgraph --image langchain/opengpts-langgraph:0.1.9 --region us-central1 --project langchain-dev --env-vars-file .env.gcp.yaml
 
-docker build -t langchain/opengpts-backend:0.1.8 --platform linux/amd64,linux/arm64 .
-docker push langchain/opengpts-backend:0.1.8
-gcloud beta run deploy opengpts-demo-backend --image langchain/opengpts-backend:0.1.8 --region us-central1 --project langchain-dev --env-vars-file .env.gcp.yaml
+docker build -t langchain/opengpts-backend:0.1.9 --platform linux/amd64,linux/arm64 .
+docker push langchain/opengpts-backend:0.1.9
+gcloud beta run deploy opengpts-demo-backend --image langchain/opengpts-backend:0.1.9 --region us-central1 --project langchain-dev --env-vars-file .env.gcp.yaml
